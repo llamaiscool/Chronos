723a4fd3972b86432249710afac39caca1fcc208
wiseLlama0 <wisellama.ch@gmail.com>
2023-08-13 23:10:00 -0400

    update intents

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const client = new Discord.Client({
     intents: [
       Discord.GatewayIntentBits.Guilds,
       Discord.GatewayIntentBits.GuildMessages,
+      Discord.GatewayIntentBits.MessageContent
     ]
   })
 
